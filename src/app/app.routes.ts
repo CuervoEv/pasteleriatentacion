7dcb9b1b4f647f9eebb2f1213cd0e3545a3aa444
CuervoEv <juliandavidcuervomartinez@gmail.com>
2025-10-28 14:08:47 -0500

    Primer commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Products } from './page/products/products';
+import { About } from './page/about/about';
+import { Home } from './page/home/home';
+import { Contact } from './page/contact/contact';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path: '', component:Home},
+    {path: 'nosotros', component:About},
+    {path: 'productos', component:Products},
+    {path: 'contacto', component:Contact},
+    {path: '**', redirectTo: ''}
+];
