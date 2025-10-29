2bc6d97e1e2810cf91a1b7715cef5a968f320597
CuervoEv <juliandavidcuervomartinez@gmail.com>
2025-10-29 13:44:35 -0500

    gghhf

--- src/app/page/contact/contact.ts
+++ src/app/page/contact/contact.ts
@@ -1,12 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule, CommonModule, RouterModule],
   templateUrl: './contact.html',
-  styleUrl: './contact.css'
+  styleUrls: ['./contact.css']
 })
 export class Contact {
-
+  onSubmit(form: any) {
+    if (form.valid) {
+      console.log('Formulario enviado', form.value);
+    }
+  }
 }
